b984001b69736e55303339f8909f52ae361acd26
Adrián Lima García <71635056+adrilgrc@users.noreply.github.com>
2024-11-06 20:13:40 +0000

    fix: styles

--- src/components/DisplayMatrix.vue
+++ src/components/DisplayMatrix.vue
@@ -1,34 +1,31 @@
 <!-- MatrixDisplay.vue -->
 <template>
   <div :class="['content-container', { 'wide-container': displayMatrix }]">
-    <button @click="toggleDisplay">Mostrar / Ocultar Matriz</button>
-    
-    <div v-if="display" class="matrix-container">
-      <table>
-        <thead>
-          <tr>
-            <!-- Celda vacía para el encabezado de la primera columna (filas de documentos) -->
-            <th></th>
-            <!-- Crear encabezados basados en el array `columnHeaders` -->
-            <th v-for="(header, index) in this.words" :key="`header-${index}`">
-              {{ header }}
-            </th>
-          </tr>
-        </thead>
-        
-        <tbody>
-          <!-- Crear las filas de la matriz -->
-          <tr v-for="(row, rowIndex) in matrix" :key="`row-${rowIndex}`">
-            <!-- Etiqueta de fila -->
-            <th>{{ `Document ${rowIndex + 1}` }}</th>
-            <!-- Celdas de datos -->
-            <td v-for="(cell, colIndex) in row" :key="`cell-${rowIndex}-${colIndex}`">
-              {{ cell }}
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <button @click="toggleDisplay">Mostrar / Ocultar {{ this.matrixName }}</button>
+    <table v-if="display" class="matrix-container">
+      <thead>
+        <tr>
+          <!-- Celda vacía para el encabezado de la primera columna (filas de documentos) -->
+          <th></th>
+          <!-- Crear encabezados basados en el array `columnHeaders` -->
+          <th v-for="(header, index) in this.words" :key="`header-${index}`">
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      
+      <tbody>
+        <!-- Crear las filas de la matriz -->
+        <tr v-for="(row, rowIndex) in matrix" :key="`row-${rowIndex}`">
+          <!-- Etiqueta de fila -->
+          <th>{{ `Document ${rowIndex + 1}` }}</th>
+          <!-- Celdas de datos -->
+          <td v-for="(cell, colIndex) in row" :key="`cell-${rowIndex}-${colIndex}`">
+            {{ cell }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
